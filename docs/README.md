ce80c540f50dbba43294c00ccc21ba1fbd5333b8
Will <author@will.party>
2024-08-12 17:23:20 -0700

    Update documentation

--- docs/README.md
+++ docs/README.md
@@ -13,8 +13,9 @@ Files named `README.md` get special treatment.
 They serve as the directory index and should:
 
 - Include just enough high-level information
-  to make a person completely unfamiliar with the directory well-oriented.
+  to orient a person completely unfamiliar with the directory.
 - Provide general information related to all contents (including sub-folders)
   of that directory.
   If the information is too specific, or too general, for the whole sub-tree,
   then it probably belongs elsewhere.
+  Put it in any individual `.md` file.
--- docs/local.sh
+++ docs/local.sh
@@ -1,10 +1,16 @@
 #!bash
 
-# Move to the directory of this shell script,
-# which should be the same directory as `package.json`.
-pushd "$(dirname "$0")"
+# Bazel's hermetic rules don't work well for VitePress' dev server,
+# which watches an entire source directory and hot-reloads on all changes.
+# So we use Bazel just to download the `npm` executable,
+# then use it like normal (it will create a `node_modules/` directory, etc.).
 
-# Run NPM as normal from that directory,
-# just downloaded through Bazel for convenience.
+# Move to the directory of this shell script,
+# which should be the same directory as the VitePress site's `package.json`
+# and is also where `node_modules/` will be downloaded.
+pushd "$(dirname "$0")" > /dev/null
+
 bazel run @nodejs//:npm -- install
 bazel run @nodejs//:npm -- run docs:dev
+
+popd > /dev/null
