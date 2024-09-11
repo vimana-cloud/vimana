bfe2bc354f79a59e543f62b45885ebb5c8a4b8c1
Will <author@will.party>
2024-09-11 14:40:36 -0700

    Update utilities

--- util/error.rs
+++ util/error.rs
@@ -1,18 +1,33 @@
-
 use std::error::Error as StdError;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::result::Result as StdResult;
 
+/// The [error](StdError) type.
 pub struct Error {
+    /// Developer-facing message.
     msg: String,
+
     source: Option<Box<dyn StdError>>,
 }
 
+/// A [result](StdResult) that fails with [`Error`](Error).
+pub type Result<T> = StdResult<T, Error>;
+
 impl Error {
-    pub fn leaf(msg: String) -> Self {
-        Self { msg, source: None }
+    /// Return a new error with the given message, but no source (cause) or irritants.
+    pub fn leaf<S: Into<String>>(msg: S) -> Self {
+        Self {
+            msg: msg.into(),
+            source: None,
+        }
     }
-    pub fn wrap(msg: String, source: Box<dyn StdError>) -> Self {
-        Self { msg, source: Some(source) }
+
+    /// Return a new error with the given message and source (cause), but no or irritants.
+    pub fn wrap<S: Into<String>>(msg: S, source: Box<dyn StdError>) -> Self {
+        Self {
+            msg: msg.into(),
+            source: Some(source),
+        }
     }
 }
 
--- util/update-dependencies.sh
+++ util/update-dependencies.sh
@@ -2,55 +2,62 @@
 # based on information from Bazel Central Registry and crates.io,
 # respectively.
 #
+# Arguments:
+# - Path to the `buildozer` executable.
+#
 # Requires:
 # - curl
+# - tail
 # - jq
-#
-# Run this with `bazel run` as an `sh_binary` rule.
-# Modifies `MODULE.bazel` in place.
 
-# Use colored output if stderr (2) is a terminal (-t).
+# Format output only if stderr (2) is a terminal (-t).
 if [ -t 2 ]
 then
-  # Terminal escape codes used for colored output.
+  # https://en.wikipedia.org/wiki/ANSI_escape_code
+  reset='\033[0m' # No formatting.
+  bold='\033[1m'
   red='\033[1;31m'
   green='\033[1;32m'
-  brown='\033[1;33m'
+  yellow='\033[1;33m'
   blue='\033[1;34m'
-  reset='\033[0m' # No Color
 else
-  # Make them all empty (no color) if we're printing to a pipe.
+  # Make them all empty (no formatting) if stderr is piped.
+  reset=''
+  bold=''
   red=''
   green=''
-  brown=''
+  yellow=''
   blue=''
-  reset=''
 fi
 
 # Move to the top level of the Git Repo for this function.
-# This means the source repo becomes the working directory
-# and source files can be mutated,
-# in contrast to Bazel's usual hermeticity.
+# The source repo becomes the working directory.
+# Source files can be mutated, in contrast to Bazel's usual hermeticity.
 # https://bazel.build/docs/user-manual#running-executables
+if [ -z "$BUILD_WORKSPACE_DIRECTORY" ]
+then
+  echo >&2 -e "${red}Error$reset Run me with ${bold}bazel run$reset"
+  exit 1
+fi
 pushd "$BUILD_WORKSPACE_DIRECTORY" > /dev/null
 
-# `$1` is the path to the `buildozer` executable, relative to `bazel-bin`.
-# Use `bazel info` to construct an absolute path.
+# `$1` is relative to `bazel-bin`. Use `bazel info` to make an absolute path.
 buildozer="$(bazel info bazel-bin)/$1" 
 
 # The following creates a Buildozer command file to run a batch of commands together,
 # storing the contents in a variable.
 # Start by reading the name and version of each `bazel_dep` in `MODULE.bazel`.
-bazel_updates="$("$buildozer" "print name version" "//MODULE.bazel:%bazel_dep" |
+bazel_updates="$("$buildozer" 'print name version' '//MODULE.bazel:%bazel_dep' |
   while read line
   do
     # Break each line into two space-delimited parts.
     [[ "$line" =~ ([^ ]+)\ ([^ ]+) ]] && (
       name="${BASH_REMATCH[1]}"
       current_version="${BASH_REMATCH[2]}"
+
       # Buildozer prints `(missing)` if the `bazel_dep` has no version.
       # It's an `extern/` dependency. Just skip these.
-      [[ "$current_version" == "(missing)" ]] && exit
+      [[ "$current_version" == '(missing)' ]] && exit
 
       # URL of metadata JSON file for this dependency in BCR.
       metadata_url="https://raw.githubusercontent.com/bazelbuild/bazel-central-registry/main/modules/$name/metadata.json"
@@ -59,19 +66,22 @@ bazel_updates="$("$buildozer" "print name version" "//MODULE.bazel:%bazel_dep" |
 
       if [[ "$current_version" != "$latest_version" ]]
       then
-        echo -e "$green$name$reset $red$current_version$reset → $blue$latest_version$reset" >&2
+        echo >&2 -e "$green$name$reset $red$current_version$reset → $blue$latest_version$reset"
         echo "replace version $current_version $latest_version|//MODULE.bazel:$name"
       fi
     )
   done
 )"
 
-# https://doc.rust-lang.org/cargo/reference/registry-index.html#index-files
 # $1 is a package name on crates.io.
-function crate_index_folder {
-  if (( ${#1} <= 2 )); then
+# Print the folder part of the URL.
+# https://doc.rust-lang.org/cargo/reference/registry-index.html#index-files
+function crate-index-folder {
+  if (( ${#1} <= 2 ))
+  then
     echo "${#1}"
-  elif (( ${#1} == 3)); then
+  elif (( ${#1} == 3))
+  then
     echo "3/${1:0:1}"
   else
     echo "${1:0:2}/${1:2:2}"
@@ -94,18 +104,19 @@ rust_updates="$("$buildozer" "print startline package version" "//MODULE.bazel:%
       current_version="${BASH_REMATCH[3]}"
       
       # URL of metadata JSON for this crate on crates.io.
-      index_url="https://index.crates.io/$(crate_index_folder "$name")/$name"
+      index_url="https://index.crates.io/$(crate-index-folder "$name")/$name"
       # Get the latest version from the index.
       latest_version="$(curl --silent "$index_url" | tail --lines=1 | jq --raw-output '.vers')"
 
       if [[ "$current_version" != "$latest_version" ]]
       then
-        echo -e "$brown$name$reset $red$current_version$reset → $blue$latest_version$reset" >&2
+        echo >&2 -e "$yellow$name$reset $red$current_version$reset → $blue$latest_version$reset"
         echo "replace version $current_version $latest_version|//MODULE.bazel:%$line_number"
       fi
     )
   done)"
 
+# Run all updates in a single Buildozer command file.
 echo -e "$bazel_updates\n$rust_updates" | "$buildozer" -f -
 
 # Go back to the initial working directory, because why not.
