acf33d33b2dbddcb29490a08a6f87a4732e1b7c2
Will <author@will.party>
2024-11-19 14:30:06 -0800

    Refactor K8s rules into separate module

--- e2e/placeholder.sh
+++ e2e/placeholder.sh
@@ -1,2 +1,10 @@
 
 grpcurl api.vimana.host:61803 vimana.api.v1.Domains/Create
+sleep 0.25
+grpcurl api.vimana.host:61803 vimana.api.v1.Domains/Create
+sleep 0.25
+grpcurl api.vimana.host:61803 vimana.api.v1.Domains/Create
+sleep 0.25
+grpcurl api.vimana.host:61803 vimana.api.v1.Domains/Create
+
+exit 1
