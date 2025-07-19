533837ef590e560d59499b8d2c80474aa61d471f
Will Noble <author@will.party>
2025-07-19 10:53:09 -0700

    Allow containerzed build in the default VSCode build action

--- dev/file.sh
+++ dev/file.sh
@@ -15,12 +15,16 @@ source 'dev/bash-util.sh'
 
 action="$1"  # Either 'build' or 'test'.
 path="$2"    # Source file path relative to the workspace directory.
+shift 2
+
+# Set `BAZEL=./.bin/bazel-docker` to run builds and tests in a container.
+bazel="${BAZEL:-bazel}"
 
 case "$action" in
   build)
     # Read all direct reverse dependencies in the same package (directory) as the source file
     # into an array.
-    readarray -t targets < <(bazel query "same_pkg_direct_rdeps($path)")
+    readarray -t targets < <("$bazel" query "same_pkg_direct_rdeps($path)")
 
     target_count=${#targets[@]}
     if (( target_count == 0 ))
@@ -29,14 +33,14 @@ case "$action" in
       exit 1
     fi
 
-    exec bazel build "${targets[@]}"
+    exec "$bazel" build "${targets[@]}"
     ;;
   test)
     # Select only the test targets
     # out of the union of the direct reverse dependencies
     # plus any target that directly depends on any of those.
     readarray -t targets < <( \
-      bazel query \
+      "$bazel" query \
         "tests(same_pkg_direct_rdeps($path) + rdeps(//..., same_pkg_direct_rdeps($path), 1))" \
     )
 
@@ -47,7 +51,7 @@ case "$action" in
       exit 1
     fi
 
-    exec bazel test "${targets[@]}"
+    exec "$bazel" test "${targets[@]}"
     ;;
   *)
     log-error "Unrecognized action ${bold}${action}${reset}"
