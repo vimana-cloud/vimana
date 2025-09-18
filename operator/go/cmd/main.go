5fce06864c2c2d51b47b0b9797bb46b685089518
Will Noble <author@will.party>
2025-09-18 15:28:59 -0700

    Initialize Server CRD

--- operator/go/cmd/main.go
+++ operator/go/cmd/main.go
@@ -216,6 +216,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Domain")
 		os.Exit(1)
 	}
+	if err = (&controller.ServerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Server")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
