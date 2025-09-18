741a75e0fd5d85f4f1dd6c5da4693790d2b8e95e
Will Noble <author@will.party>
2025-09-18 15:25:29 -0700

    Initialize Domain CRD

--- operator/go/cmd/main.go
+++ operator/go/cmd/main.go
@@ -209,6 +209,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Vimana")
 		os.Exit(1)
 	}
+	if err = (&controller.DomainReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Domain")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
