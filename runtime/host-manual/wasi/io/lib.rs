e8ba6bd0bee318b3b14c489ccdbc13cc99d59008
Will Noble <author@will.party>
2025-12-09 14:34:00 -0800

    fix: Remove broken experimental host implementation

--- runtime/host-manual/wasi/io/lib.rs
+++
@@ -1,8 +0,0 @@
-mod error;
-mod streams;
-
-use error::Error;
-use provide::{HostState, Provider, api};
-
-#[api(Error)]
-pub struct Io;
--- runtime/host-manual/wasi/lib.rs
+++
@@ -1,5 +0,0 @@
-use io::Io;
-use provide::{HostState, Provider, api};
-
-#[api(Io)]
-pub struct Wasi;
